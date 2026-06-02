ec5ad647e32d24413904a67dcd7208fbb6ebdbd0
peize <liupeize@boosterobotics.com>
2026-06-02 15:11:26 +0800

    feat: support 24

--- install.sh
+++ install.sh
@@ -12,6 +12,21 @@ echo "CPU Arch=$cpu_arch"
 third_party_dir=$booster_sdk_dir/third_party
 echo "Third Party Dir = $third_party_dir"
 
+# Perform version check early to avoid wasting time on apt update for unsupported systems
+ubuntu_version=$(lsb_release -rs)
+case $ubuntu_version in
+    24.*) ubuntu_version_flag=24 ;;
+    22.*) ubuntu_version_flag=22 ;;
+    *) 
+        # Print a red error message and exit if the version is not 22.* or 24.*
+        echo -e "\033[31m[ERROR] Unsupported Ubuntu version: $ubuntu_version \033[0m"
+        echo -e "\033[31m[HINT] This SDK only supports Ubuntu 22.04 and above. Installation aborted.\033[0m"
+        exit 1 
+        ;;
+esac
+
+echo "Ubuntu Version = $ubuntu_version_flag"
+
 set -e
 
 apt update
@@ -23,22 +38,8 @@ apt install -y libssl-dev
 apt install -y libasio-dev
 apt install -y libtinyxml2-dev
 
-ubuntu_version=$(lsb_release -rs)
-ubuntu_version_flag=20
-case $ubuntu_version in
-    22.*) ubuntu_version_flag=22 ;;
-    20.*) ubuntu_version_flag=20 ;;
-    18.*) ubuntu_version_flag=18 ;;
-    *) ubuntu_version_flag=0 ;;
-esac
-
-if [ $ubuntu_version_flag -eq 22 ]; then
-    booster_sdk_lib_dir=$booster_sdk_dir/lib/$cpu_arch
-    third_party_lib_dir=$third_party_dir/lib/$cpu_arch
-else
-    booster_sdk_lib_dir=$booster_sdk_dir/lib/$cpu_arch/$ubuntu_version_flag
-    third_party_lib_dir=$third_party_dir/lib/$cpu_arch/$ubuntu_version_flag
-fi
+booster_sdk_lib_dir=$booster_sdk_dir/lib/$cpu_arch/$ubuntu_version_flag
+third_party_lib_dir=$third_party_dir/lib/$cpu_arch/$ubuntu_version_flag
 
 echo "SDK Lib Dir = $booster_sdk_lib_dir"
 echo "Third Party Lib Dir = $third_party_lib_dir"
